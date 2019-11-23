fc0be7a833f57bd5ec6141d2ba5fa9a9d09fb0f1
Eathox <Rendier3636@gmail.com>
2019-11-24 00:10:59 +0100

    Made every library dep an seprate class

--- setup.py
+++ setup.py
@@ -3,37 +3,43 @@
 
 from sys import argv
 
-from setup import setup_const
+from setup import libraries
+
+dependencies = [
+	"numpy",
+	"pygame",
+	"pyopengl",
+]
 
 def _print_action_result(success, libraries, action_str):
-    """print install or uninstall action results"""
-    if success == False:
-        print(f"Failed to {action_str} all dependencies")
-    if libraries == [] and success == True:
-        print(f"All dependencies allready {action_str}.")
-    elif libraries != []:
-        print(f"\nSuccessfully {action_str} the following:")
-        for library in libraries:
-            print(f" - {library}")
+	"""print install or uninstall action results"""
+	if success == False:
+		print(f"Failed to {action_str} all dependencies")
+	if libraries == [] and success == True:
+		print(f"All dependencies allready {action_str}.")
+	elif libraries != []:
+		print(f"\nSuccessfully {action_str} the following:")
+		for library in libraries:
+			print(f" - {library}")
 
 if __name__ == "__main__":
-    """Setup dependencies"""
-    setup = setup_const.Setup(argv[1:])
-    if setup.pip_installed() == False:
-        suggestion = "Unrecognized kernel, unable to provide install suggestion."
-        if setup.platform == "linux":
-            suggestion = "Consider runing: 'sudo apt install python3-pip' to install."
-        elif setup.platform == "darwin":
-            suggestion = "Consider runing: 'brew install python3' to install."
-        elif setup.platform == "win32":
-            suggestion = "Consider runing: 'python3 get-pip.py' to install."
-        print(f"{setup.pip} is not installed.\n{suggestion}")
-        exit()
+	"""Setup dependencies"""
+	setup = libraries.Setup(argv[1:], *dependencies)
+	if setup.pip_installed() == False:
+		suggestion = "Unrecognized kernel, unable to provide install suggestion."
+		if setup.platform == "linux":
+			suggestion = "Consider runing: 'sudo apt install python3-pip' to install."
+		elif setup.platform == "darwin":
+			suggestion = "Consider runing: 'brew install python3' to install."
+		elif setup.platform == "win32":
+			suggestion = "Consider runing: 'python3 get-pip.py' to install."
+		print(f"{setup.pip} is not installed.\n{suggestion}")
+		exit()
 
-    if "--uninstall" in setup.args or "--remove" in setup.args:
-        success, libraries = setup.uninstall_libaries()
-        action_str = "uninstalled"
-    else:
-        success, libraries = setup.install_libaries()
-        action_str = "installed"
-    _print_action_result(success, libraries, action_str)
+	if "--uninstall" in setup.args or "--remove" in setup.args:
+		success, libraries = setup.uninstall_libraries()
+		action_str = "uninstalled"
+	else:
+		success, libraries = setup.install_libraries()
+		action_str = "installed"
+	_print_action_result(success, libraries, action_str)
---
+++ setup/libraries.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+"""Common setup constants"""
+
+from sys import platform
+from subprocess import run, PIPE
+
+def _run_pip(pip, arguments, silent=False):
+	"""Runs pip with arguments and returns success"""
+	if silent == True:
+		success = run([pip] + arguments, stdout=PIPE, stderr=PIPE).returncode
+	else:
+		success = run([pip] + arguments).returncode
+	return success == 0
+
+class Library:
+	"""A single library dependency"""
+
+	def __init__(self, name, pip="pip3"):
+		"""Define library with given pip"""
+		self.platform = platform.lower()
+		self.name = name
+		self.pip = pip
+
+	def __str__(self):
+		"""Return library name"""
+		name = self.__class__.__name__
+		return f"<{name} - {self.name}>"
+
+	def library_installed(self):
+		"""Checks if the library is installed using shell"""
+		arguments = ["show", self.name]
+		return _run_pip(self.pip, arguments, True)
+
+	def install_library(self):
+		"""Installs library"""
+		print(f"Installing {self.name}")
+		arguments = ["install", self.name]
+		success = _run_pip(self.pip, arguments)
+		if success == False:
+			print(f"Failed to install {self.name}")
+			return False
+		return success
+
+	def uninstall_library(self):
+		"""Uninstalls library"""
+		arguments = ["uninstall", self.name]
+		success = _run_pip(self.pip, arguments)
+		removed = self.library_installed() == False
+		if success == False:
+			return False, removed
+		return success, removed
+
+class Setup:
+	"""Manage all library dependencies"""
+
+	def __init__(self, args, *libraries, pip="pip3"):
+		"""Add args and collect all installed libraries"""
+		self.platform = platform.lower()
+		self.args = list(map(str.lower, args))
+		self.pip = pip
+		self.libraries = []
+		for name in libraries:
+			self.libraries.append(Library(name, pip))
+
+	def __str__(self):
+		"""Return args and libraries in string form"""
+		name = self.__class__.__name__
+		library_names = []
+		for library in self:
+			library_names.append(str(library))
+		library_names = " ".join(library_names)
+		return f"<{name} - {self.args}, ({library_names})>"
+
+	def __iter__(self):
+		"""Define iterator"""
+		return self.libraries.__iter__()
+
+	def pip_installed(self):
+		"""Checks if pip3 is installed"""
+		arguments = ["-V"]
+		return _run_pip(self.pip, arguments, True)
+
+	def install_libraries(self):
+		"""Installs all libraries"""
+		installed = []
+		for library in self:
+			if library.library_installed() == True:
+				continue
+			success = library.install_library()
+			if success == False or library.library_installed() == False:
+				return False, installed
+		return True, installed
+
+	def uninstall_libraries(self):
+		"""Uninstalls all libraries"""
+		uninstalled = []
+		for library in self:
+			if library.library_installed() == False:
+				continue
+			success, removed = library.uninstall_library()
+			if success == False:
+				return False, uninstalled
+		return True, uninstalled
