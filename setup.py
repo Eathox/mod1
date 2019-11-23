3b35f6536e8180b510a148f99eac80d0aa5dd3b4
Eathox <Rendier3636@gmail.com>
2019-11-24 00:27:32 +0100

    made setup suggestion a map, and added pip website

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ from sys import argv
 
 from setup import libraries
 
-dependencies = [
+g_dependencies = [
 	"numpy",
 	"pygame",
 	"pyopengl",
 ]
+
+g_suggestions = {
+	"linux": "Consider runing: 'sudo apt install python3-pip' to install.",
+	"darwin": "Consider runing: 'brew install python3' to install.",
+	"win32": "Consider runing: 'python3 get-pip.py' to install.",
+}
 
 def _print_action_result(success, libraries, action_str):
 	"""print install or uninstall action results"""
@@ -24,16 +30,14 @@ def _print_action_result(success, libraries, action_str):
 
 if __name__ == "__main__":
 	"""Setup dependencies"""
-	setup = libraries.Setup(argv[1:], *dependencies)
+	setup = libraries.Setup(argv[1:], *g_dependencies)
 	if setup.pip_installed() == False:
-		suggestion = "Unrecognized kernel, unable to provide install suggestion."
-		if setup.platform == "linux":
-			suggestion = "Consider runing: 'sudo apt install python3-pip' to install."
-		elif setup.platform == "darwin":
-			suggestion = "Consider runing: 'brew install python3' to install."
-		elif setup.platform == "win32":
-			suggestion = "Consider runing: 'python3 get-pip.py' to install."
+		if setup.platform in g_suggestions:
+			suggestion = g_suggestions[setup.platform]
+		else:
+			suggestion = "Unrecognized kernel, unable to provide install suggestion."
 		print(f"{setup.pip} is not installed.\n{suggestion}")
+		print(f"For more info visit: https://pip.pypa.io/en/stable/installing/")
 		exit()
 
 	if "--uninstall" in setup.args or "--remove" in setup.args:
