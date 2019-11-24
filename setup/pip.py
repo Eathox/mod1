668acc8a90e3c6f182d0408ea4cc5ddb6dd694ba
pholster <pholster@student.codam.nl>
2019-11-24 14:46:26 +0100

    cleaned up pip.py

--- setup/pip.py
+++ setup/pip.py
@@ -6,7 +6,8 @@ from subprocess import run, PIPE
 def run_pip(pip, arguments, silent=False):
 	"""Runs pip with arguments and returns success"""
 	if silent == True:
-		success = run([pip] + arguments, stdout=PIPE, stderr=PIPE).returncode
+		result = run([pip] + arguments, stdout=PIPE, stderr=PIPE)
 	else:
-		success = run([pip] + arguments).returncode
+		result = run([pip] + arguments)
+	success = result.returncode
 	return success == 0
