51606443960fdfa9a19bb37794fca4b14953e296
pholster <pholster@student.codam.nl>
2019-11-24 16:21:12 +0100

    removing validation from terrain file

--- src/terrain.py
+++ src/terrain.py
@@ -24,57 +24,6 @@ class Terrain:
         self.loc = file_path
         self.file = Path(file_path)
 
-def _validate_line(line, row, terrain):
-    """Validates a single lines format"""
-    line = sub("[ \t]+", " ", line) # Compress multipale spacers to single space
-    if len(line) < 1:
-        return f"Line {row} is empty"
-
-    match = search(r"^([-]?\d[ ]?)+\n?", line) # match digits with space in between
-    match = match.group()
-    if len(match) != len(line):
-        return f"Line {row} contains none digits"
-
-    match = match.strip("\n")
-    if match.endswith(" "):
-        return f"Line {row} has trailing whitespace"
-
-    count = 1
-    points = match.split()
-    terrain.points += [points]
-    for point in points:
-        point = int(point)
-        if count > MAX_SIZE:
-            return f"Line {row} exceeds max size ({MAX_SIZE})"
-        elif point < MIN_HEIGHT:
-            return f"Line {row} '{point}' is smaller then min size ({MIN_HEIGHT})"
-        elif point > MAX_HEIGHT:
-            return f"Line {row} '{point}' is bigger then max size ({MAX_HEIGHT})"
-        count += 1
-    return ""
-
-def _validate_map_file(terrain):
-    """Validates file format"""
-    if terrain.loc.endswith(".mod1") == False:
-        return "Invalid file extension expected '.mod1'"
-    elif terrain.file.exists() == False:
-        return "No such file"
-
-    row = 1
-    content = terrain.file.read_text()
-    content = content.strip()
-    content = content.splitlines()
-    for line in content:
-        terrain.error = _validate_line(line, row, terrain)
-        if row > MAX_SIZE:
-            return f"Lines in file exceeds max size ({MAX_SIZE})"
-        elif terrain.error != "":
-            return terrain.error
-        row += 1
-    if len(terrain.points) == 0:
-        return "File is empty"
-    return terrain.error
-
 def _grid_to_3d(terrain):
     min_height = abs(MIN_HEIGHT)
     for layer in range(terrain.height_3d):
